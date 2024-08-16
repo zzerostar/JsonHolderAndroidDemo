758ba389d2ac4202dc6a64063ca0cb114af8cc0d
zero <zzerostar@163.com>
2024-08-16 15:40:47 +0800

    Change the project dictionary to do preparation for flutter

---
+++ JsonHolderAndroid/app/src/main/java/com/ziano/jsonholderandroid/old/ui/adapter/Test.kt
@@ -0,0 +1,11 @@
+package com.ziano.jsonholderandroid.old.ui.adapter
+
+/**
+ * @author zz
+ * @email zzerostar@163.com
+ * @date 2024/8/6
+ * @desc
+ */
+interface Test {
+//    val x = 0
+}
---
+++ JsonHolderAndroid/app/src/test/java/com/ziano/jsonholderandroid/compose/vm/PostDetailViewModelTest.kt
@@ -0,0 +1,34 @@
+package com.ziano.jsonholderandroid.compose.vm
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+/**
+ * @author zz
+ * @email zzerostar@163.com
+ * @date 2024/8/3
+ * @desc
+ */
+class PostDetailViewModelTest {
+
+    @Test
+    fun getPostDetail() {
+    }
+
+    @Test
+    fun getPostComments() {
+    }
+
+    @Test
+    fun initState() {
+    }
+
+    @Test
+    fun handleIntent() {
+    }
+
+    @Test
+    fun getPostRepository() {
+    }
+}
